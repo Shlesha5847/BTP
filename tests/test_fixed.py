7c2e526903491d4c57c2ccaa8da95dbba584c393
AdvikaTijare10 <112315013@cse.iiitp.ac.in>
2025-11-27 12:33:58 +0000

    final

--- tests/test_fixed.py
+++
@@ -1,8 +0,0 @@
-
-
-from cpabe.fixed.scheme import SensorsCPABEFixedIDBound
-from tests.test_common import run_common_test
-
-if __name__ == "__main__":
-    scheme = SensorsCPABEFixedIDBound()
-    run_common_test(scheme, is_fixed=True)
